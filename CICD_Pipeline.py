eb0821528a7a3a43d50d221144508cd965102a8b
Jyoti Gupta <jyotigupta200028@gmail.com>
2025-07-31 16:31:56 +0530

    Update

--- CICD_Pipeline.py
+++ CICD_Pipeline.py
@@ -4,7 +4,7 @@
 
 def extract_data():
     print("Extracting data...")
-    data = [10, 20, 30, 40, 50,60,100,200]
+    data = [10, 20, 30, 40, 50,60,100,200,300]
     return data
 
 def transform_data(data):
