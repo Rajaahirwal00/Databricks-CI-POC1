0fe2829689ec17abcc25fafbfdb1926b2959e25d
Rajaahirwal00 <Rajaahirwal00>
2025-08-04 20:37:35 +0000

    update

--- deploy.bat
+++ deploy.bat
@@ -4,8 +4,8 @@ echo === AZURE_CLIENT_ID: %AZURE_CLIENT_ID%
 echo === AZURE_CLIENT_SECRET: %AZURE_CLIENT_SECRET%
 echo === AZURE_TENANT_ID: %AZURE_TENANT_ID%
 
-call step1_azure_login.bat
-call step2_acr_login.bat
-call step3_docker_build.bat
-call step4_docker_push.bat
-call step5_k8s_deploy.bat
+call azure_login.bat
+call acr_login.bat
+call docker_build.bat
+call docker_push.bat
+call k8s_deploy.bat
